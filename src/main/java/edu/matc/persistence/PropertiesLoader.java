28a23810c817f3cb00d35b1f451d99e2892daf0e
Alivia Kleinfeldt <alivia.kleinfeldt@gmail.com>
2021-09-14 13:46:23 -0500

    add properties loader

---
+++ src/main/java/edu/matc/persistence/PropertiesLoader.java
@@ -0,0 +1,35 @@
+package edu.matc.persistence;
+
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * This interface contains a default method that can be used anywhere a Properties
+ * object is needed to be loaded.
+ * @author Eric Knapp
+ *
+ */
+public interface PropertiesLoader {
+
+    /**
+     * This default method will load a properties file into a Properties instance
+     * and return it.
+     * @param propertiesFilePath a path to a file on the java classpath list
+     * @return a populated Properties instance or an empty Properties instance if
+     * the file path was not found.
+     */
+    default Properties loadProperties(String propertiesFilePath) {
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (IOException ioException) {
+            System.out.println("There was a problem inputting the properties file");
+            ioException.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("There was a problem with the properties file");
+            exception.printStackTrace();
+        }
+        return properties;
+    }
+}
